2bdb7c741e06e7ea5a9db97e503e6d8d1271d582
Seth <seth@Zipcoders-MBP.home>
2023-03-14 22:19:12 -0400

    wrote selection

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Music {
 
     private String[] playList;
@@ -9,6 +11,25 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        ArrayList<Integer> targets = new ArrayList<>();
+        int variance = Integer.MAX_VALUE;
+        for (int i = 0; i < playList.length; i++) {
+            if(playList[i].equals(selection)){
+                targets.add(i);
+            }
+
+        }
+        
+        targets.add(targets.get(targets.size()-1) - playList.length);
+        targets.add(targets.get(0)+ playList.length);
+
+        for(Integer i:targets){
+            if(Math.abs(startIndex-i)<variance){
+                variance = Math.abs(startIndex-i);
+            }
+        }
+
+
+        return variance;
     }
 }
